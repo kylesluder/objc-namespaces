4c01fd3a57703c35166defa39e372d4699058794
Kyle Sluder <kyle@ksluder.com>
2014-02-02 19:51:02 -0800

    Adding convenience methods for pulling subtrees.

--- SetupSubtrees.sh
+++ SetupSubtrees.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
-git remote add llvm-mirror https://github.com/llvm-mirror/llvm.git
-git remote add clang-mirror https://github.com/llvm-mirror/clang.git
-git remote add compiler-rt-mirror https://github.com/llvm-mirror/compiler-rt.git
+COMMAND="$1"
 
-git subtree add --prefix=llvm llvm-mirror master
-git subtree add --prefix=llvm/tools/clang clang-mirror master
-git subtree add --prefix=llvm/projects/compiler-rt compiler-rt-mirror master
+case $COMMAND in
+  "setup")
+    git subtree add --prefix=llvm https://github.com/llvm-mirror/llvm.git master
+    git subtree add --prefix=llvm/tools/clang https://github.com/llvm-mirror/clang.git master
+    git subtree add --prefix=llvm/projects/compiler-rt https://github.com/llvm-mirror/compiler-rt.git master
+  ;;
+  "pull")
+    git subtree merge --prefix=llvm master
+    git subtree merge --prefix=llvm/tools/clang master
+    git subtree merge --prefix=llvm/projects/compiler-rt master
+  ;;
+  *)
+    echo >&2 "usage: `basename $0` [setup|pull]"
+    exit 1
+  ;;
+esac
+  
