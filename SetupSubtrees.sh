ed3dff61bdd0aa29d5443f736cf7def625268f1f
Kyle Sluder <kyle@ksluder.com>
2014-02-05 22:22:42 -0800

    Use the official LLVM Git mirrors instead of the unofficial GitHub ones, and use the remote refs instead of specifying the repo every time.

--- SetupSubtrees.sh
+++ SetupSubtrees.sh
@@ -4,14 +4,22 @@ COMMAND="$1"
 
 case $COMMAND in
   "setup")
-    git subtree add --prefix=llvm https://github.com/llvm-mirror/llvm.git master
+    git remote add -f llvm-mirror http://llvm.org/git/llvm.git
+    git subtree add --prefix=llvm llvm-mirror/master
+
+    git remote add -f clang-mirror http://llvm.org/git/clang.git
     git subtree add --prefix=llvm/tools/clang https://github.com/llvm-mirror/clang.git master
+
+    git remote add -f compiler-rt-mirror http://llvm.org/git/compiler-rt.git
     git subtree add --prefix=llvm/projects/compiler-rt https://github.com/llvm-mirror/compiler-rt.git master
-  ;;
+
+    git config remotes.llvm-mirrors "llvm-mirror clang-mirror compiler-rt-mirror"
+    ;;
   "pull")
-    git subtree merge --prefix=llvm master
-    git subtree merge --prefix=llvm/tools/clang master
-    git subtree merge --prefix=llvm/projects/compiler-rt master
+    git remote update llvm-mirrors
+    git subtree merge --prefix=llvm llvm-mirror/master
+    git subtree merge --prefix=llvm/tools/clang clang-mirror/master
+    git subtree merge --prefix=llvm/projects/compiler-rt compiler-rt-mirror/master
   ;;
   *)
     echo >&2 "usage: `basename $0` [setup|pull]"
