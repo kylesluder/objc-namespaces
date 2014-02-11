b863f562ca1e7f00179126512a457cfaedb041b0
Kyle Sluder <kyle@ksluder.com>
2014-02-11 14:41:28 -0800

    Split out creation of remotes from subtrees.
    Clones of the repo already have the subtrees set up, but they need their remotes set up independently.

--- SetupSubtrees.sh
+++ SetupSubtrees.sh
@@ -3,18 +3,18 @@
 COMMAND="$1"
 
 case $COMMAND in
-  "setup")
+  "setup-remotes")
     git remote add -f llvm-mirror http://llvm.org/git/llvm.git
-    git subtree add --prefix=llvm llvm-mirror/master
-
     git remote add -f clang-mirror http://llvm.org/git/clang.git
-    git subtree add --prefix=llvm/tools/clang https://github.com/llvm-mirror/clang.git master
-
     git remote add -f compiler-rt-mirror http://llvm.org/git/compiler-rt.git
-    git subtree add --prefix=llvm/projects/compiler-rt https://github.com/llvm-mirror/compiler-rt.git master
 
     git config remotes.llvm-mirrors "llvm-mirror clang-mirror compiler-rt-mirror"
-    ;;
+  ;;
+  "setup-subtree")
+    git subtree add --prefix=llvm llvm-mirror/master
+    git subtree add --prefix=llvm/tools/clang https://github.com/llvm-mirror/clang.git master
+    git subtree add --prefix=llvm/projects/compiler-rt https://github.com/llvm-mirror/compiler-rt.git master
+  ;;
   "pull")
     git remote update llvm-mirrors
     git subtree merge --prefix=llvm llvm-mirror/master
@@ -22,7 +22,7 @@ case $COMMAND in
     git subtree merge --prefix=llvm/projects/compiler-rt compiler-rt-mirror/master
   ;;
   *)
-    echo >&2 "usage: `basename $0` [setup|pull]"
+    echo >&2 "usage: `basename $0` [setup-remotes|setup-subtree|pull]"
     exit 1
   ;;
 esac
