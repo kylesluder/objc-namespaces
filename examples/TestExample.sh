a37defa939f891801d635663a26cf098b0625f33
Kyle Sluder <kyle@ksluder.com>
2014-02-17 17:51:39 -0800

    Make TestExample load clang in lldb with -g.

--- examples/TestExample.sh
+++ examples/TestExample.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
-../llvm/ObjRoot/Debug+Asserts/bin/clang -isysroot `xcrun --show-sdk-path` -fsyntax-only $*
+CLANG=../llvm/ObjRoot/Debug+Asserts/bin/clang
+CLANG_ARGS=("-cc1" "-triple" "x86_64-apple-macosx10.9.0" "-fsyntax-only" "-disable-free" "-main-file-name" "namespace.m" "-mrelocation-model" "pic" "-pic-level" "2" "-mdisable-fp-elim" "-masm-verbose" "-munwind-tables" "-target-cpu" "core2" "-target-linker-version" "224.1" "-gdwarf-2" "-resource-dir" "/Users/Shared/objc-namespaces/llvm/ObjRoot/Debug+Asserts/bin/../lib/clang/3.5" "-isysroot" "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk" "-fdebug-compilation-dir" "/Users/Shared/objc-namespaces/examples" "-ferror-limit" "19" "-fmessage-length" "204" "-stack-protector" "1" "-mstackrealign" "-fblocks" "-fobjc-runtime=macosx-10.9.0" "-fencode-extended-block-signature" "-fobjc-exceptions" "-fexceptions" "-fdiagnostics-show-option" "-fcolor-diagnostics" "-vectorize-slp" "-x" "objective-c" "$@")
+
+if [[ "$1" == "-g" ]]; then
+    exec lldb $CLANG -- $CLANG_ARGS[@]
+else
+    exec $CLANG $CLANG_ARGS[@]
+fi
